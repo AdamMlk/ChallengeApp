d42a1a2f64c35d8f338581f08c4eac50ea5b6ace
user-A <adam.mhlk@outlook.com>
2023-03-27 15:01:04 +0200

    zadanie - dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// program - home work day 4
+
+// zmienna imię
+using System.ComponentModel.Design;
+
+string name = "Ewa";
+
+// zmienna płeć
+string sex = "Kobieta";
+
+//zmienna wiek
+int age = 33;
+
+// weryfikacja
+string result = name + "," + "lat " + age;
+
+if (age < 30 && age > 18 && sex == "Kobieta")
+{
+            Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 33 && name == "Ewa" && sex == "Kobieta")
+{
+    Console.WriteLine(result);
+}
+else if (age < 18 && sex == "Mężczyzna")
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Osoba spoza zbioru");
+}
